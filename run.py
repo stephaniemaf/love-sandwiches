0d77dac7d92d08e0a36761e666fde9e05c8df5c6
stephaniemaf <farrell.s.ava@gmail.com>
2022-12-13 12:31:47 +0000

    aded while loop to continously loop untill valid data is enetred

--- run.py
+++ run.py
@@ -17,13 +17,21 @@ def get_sales_data():
     """
     Get sales figures input from user 
     """
-    print("Please enter sales data from the last market day")
-    print("Data should be six numbers, seperated by a comma")
-    print("Example: 20,30,40,50,60,70\n")
 
-    data_str = input("Enter your data here: ")
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+    while True:
+        print("Please enter sales data from the last market day")
+        print("Data should be six numbers, seperated by a comma")
+        print("Example: 20,30,40,50,60,70\n")
+
+        data_str = input("Enter your data here: ")
+
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid")
+            break
+
+    return sales_data
 
 
 def validate_data(values):
@@ -33,12 +41,16 @@ def validate_data(values):
     or if there aren't exactly 6 values."""
 
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 value required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
 
 
-get_sales_data()
+data = get_sales_data()
