db7c7dee71657ac313595ba850e62071b2fc48fa
stephaniemaf <farrell.s.ava@gmail.com>
2022-12-13 12:04:58 +0000

    added try except to validate data entered

--- run.py
+++ run.py
@@ -12,7 +12,33 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("love_sandwiches")
 
-sales = SHEET.worksheet("sales")
 
-data = sales.get_all_values()
-print(data)
+def get_sales_data():
+    """
+    Get sales figures input from user 
+    """
+    print("Please enter sales data from the last market day")
+    print("Data should be six numbers, seperated by a comma")
+    print("Example: 20,30,40,50,60,70\n")
+
+    data_str = input("Enter your data here: ")
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values."""
+
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 value required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+
+
+get_sales_data()
